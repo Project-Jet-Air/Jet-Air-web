7d435d2ec619ac186ddc684c46bd5e7dffa5b576
Matthew Hartman <hartman.737@buckeyemail.osu.edu>
2025-02-22 12:03:54 -0500

    create react app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const openMenu = () => {
+    document.querySelector(".sidebar")?.classList.add("open");
+  };
+
+  const closeMenu = () => {
+    document.querySelector(".sidebar")?.classList.remove("open");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      {/* Header */}
+      <header className="header">
+        <div className="brand">
+          <button onClick={openMenu}>&#9776;</button>
+          <a href="#">Jet Air</a>
+        </div>
+        <div className="header-links">
+          <a href="#">Cart</a>
+          <a href="#">Sign In</a>
+        </div>
       </header>
+
+      {/* Sidebar */}
+      <aside className="sidebar">
+        <h3>Shopping Categories</h3>
+        <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+        <ul>
+          <li><a href="#">Pants</a></li>
+          <li><a href="#">Shirts</a></li>
+        </ul>
+      </aside>
+
+      {/* Main Content */}
+      <main>
+        <div className="content">
+          <ul className="products">
+            {[...Array(2)].map((_, index) => (
+              <li key={index}>
+                <div className="product">
+                <img className="product-image" src="/images/d1.jpg" alt="product" />
+
+                  <div className="product-name">
+                    <a href="#">Slim Shirt</a>
+                  </div>
+                  <div className="product-brand">Nike</div>
+                  <div className="product-price">$60</div>
+                  <div className="product-rating">4.5 Stars (10 Reviews)</div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </main>
+
+      {/* Footer */}
+      <footer>&copy; 2022 Jet Air</footer>
     </div>
   );
 }
