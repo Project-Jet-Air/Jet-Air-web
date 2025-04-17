14173b9c9126a996176d813860a1d309290d4465
Matthew Hartman <hartman.737@buckeyemail.osu.edu>
2025-04-17 16:56:07 -0400

    Connect to API and fetch products

--- frontend/src/Products/Products.tsx
+++ frontend/src/Products/Products.tsx
@@ -1,12 +1,46 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+type Product = {
+    id: number;
+    name: string;
+    description: string;
+    brand: string;
+    price: number;
+    rating: number;
+    numberOfReviews: number;
+    imageUrl: string;
+};
+
+const emptyProducts: Product[] = [];
 
 const Products = () => {
-  return (
-    <div>
-      <h1>Catalog</h1>
-      <p>Browse our amazing collection!</p>
-    </div>
-  );
+    const [products, setProducts] = useState<Product[]>(emptyProducts);
+
+    useEffect(() => {
+        axios.get<Product[]>("http://localhost:5263/catalog", {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+        .then((response) => setProducts(response.data))
+        .catch((error) => console.log(error));
+    }, []);
+
+    return (
+        <div>
+            <h1>Catalog</h1>
+            <p>Browse our amazing collection!</p>
+
+            <ul>
+                {products.map(product => (
+                    <li key={product.id}>
+                        {product.name} - ${product.price}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default Products;
